190296cf0b83ce08d29a93174cc214e64931b8db
chernandezba <chernandezba@hotmail.com>
2018-04-16 10:30:04 +0200

    mc

--- src/menu.c
+++ src/menu.c
@@ -6787,6 +6787,14 @@ menu_writing_inverse_color.v=antes_menu_writing_inverse_color.v;
                                         cls_menu_overlay();
 					menu_debug_follow_pc.v=0; //se deja de seguir pc
 					menu_debug_registers_change_ptr();
+
+        clear_putpixel_cache();
+        //TODO: Si no se pone clear_putpixel_cache,
+        //el texto se fusiona con el fondo de manera casi transparente,
+        //como si no borrase el putpixel cache
+        //esto también sucede en otras partes del código del menú pero no se por que es
+
+
                                         //Decimos que no hay tecla pulsada
                                         acumulado=MENU_PUERTO_TECLADO_NINGUNA;
                                         menu_debug_registers_ventana();
