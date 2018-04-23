6575f436aaaf8fd337f08674ee9a682ac6fb48a5
chernandezba <chernandezba@hotmail.com>
2018-04-23 20:46:43 +0200

    mc

--- src/codetests.sh
+++ src/codetests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-./zesarux --codetests
-
 mkdir /tmp/pruebas
 cp *.c /tmp/pruebas
+cp *.rom /tmp/pruebas
 
 
-for i in /tmp/pruebas/*.c; do
+for i in /tmp/pruebas/*; do
 	echo "Running compress/uncompress test for $i"
 	./zesarux --codetests $i
+	if [ $? != 0 ]; then
+		echo "ERROR"
+		exit 1
+	fi
 done
