539c378b939d99d3927609713cdb9dc9cf073a98
chernandezba <chernandezba@hotmail.com>
2018-04-24 16:56:09 +0200

    mc

--- src/manual_tests.sh
+++ src/manual_tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+echo "Probar ClckFreq.p"
+./zesarux --noconfigfile --machine zx81 extras/media/zx81/tests/ClckFreq.p
+ClckFreq.p
+
 echo "Probar chroma81"
 ./zesarux --noconfigfile --machine zx81 --realvideo --chroma81 extras/media/zx81/Chroma_81/ALMINCOL.P
 ./zesarux --noconfigfile --machine zx81 --realvideo --chroma81 extras/media/zx81/Chroma_81/GalaxCol.P
@@ -10,4 +14,7 @@ echo "Probar chroma81"
 ./zesarux --noconfigfile --machine zx81 --realvideo --chroma81 extras/media/zx81/Chroma_81/stellar.p
 ./zesarux --noconfigfile --machine zx81 --realvideo --chroma81 extras/media/zx81/Chroma_81/wall-expanded.p
 ./zesarux --noconfigfile --machine zx81 --realvideo --chroma81 extras/media/zx81/Chroma_81/rebound/Rebound.p
+
+
+
 
