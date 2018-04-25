c16cac7f0a0ac5993a9c4f790623901c7c8b7451
chernandezba <chernandezba@hotmail.com>
2018-04-25 19:16:07 +0200

    mc

--- src/manual_tests.sh
+++ src/manual_tests.sh
@@ -3,6 +3,17 @@
 echo "z80 tests. errores de flags normalmente en: BIT n,(HL). errores de memptr en ninguno"
 ./zesarux --noconfigfile extras/media/spectrum/tests/z80tests.tap
 
+echo "z80doc tests. van todos"
+./zesarux --noconfigfile ./extras/media/spectrum/tests/z80doc.tap
+
+echo "fusetest. en 48k, 128k. falla Floating bus. skipped read 3ffd, 7ffd. en +2a falla ademas LDIR, Floating bus skipped, bffd read failed. en 128k dado que lee puerto
+paginacion, se pone pantalla en negro"
+./zesarux --noconfigfile ./extras/media/spectrum/tests/fusetest.tap
+./zesarux --noconfigfile --machine 128k ./extras/media/spectrum/tests/fusetest.tap
+
+echo "Timing_Tests-48k_v1.0.sna. errores normalmente en test 4 add etc, test 25 add etc, test 36 in a etc, test 37 in a etc"
+./zesarux --noconfigfile ./extras/media/spectrum/tests/Timing_Tests-48k_v1.0.sna
+
 echo "Probar ClckFreq.p"
 ./zesarux --noconfigfile --machine zx81 extras/media/zx81/tests/ClckFreq.p
 ClckFreq.p
