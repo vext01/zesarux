4b2c1b69a6f06eb7201e68fa52887f95d9b3c2c7
chernandezba <chernandezba@hotmail.com>
2018-07-02 21:11:27 +0200

    testing zxuno dma

--- src/zxuno.c
+++ src/zxuno.c
@@ -34,7 +34,7 @@
 #include "divmmc.h"
 #include "ay38912.h"
 #include "ulaplus.h"
-
+#include "operaciones.h"
 
 z80_byte last_port_FC3B=0;
 
@@ -161,6 +161,23 @@ z80_int zxuno_dma_current_src;
 z80_int zxuno_dma_current_dst;
 z80_int zxuno_dma_current_len;
 
+
+void temp_dma_operate_memory_to_io(void)
+{
+	z80_byte dma_source_value;
+		dma_source_value=peek_byte_no_time(zxuno_dma_current_src++);
+		printf ("out port %04XH value %02XH (source %04XH, len %04XH)\n",zxuno_dma_current_dst,dma_source_value,zxuno_dma_current_src,zxuno_dma_current_len);
+		out_port_spectrum_no_time(zxuno_dma_current_dst,dma_source_value);
+
+		zxuno_dma_current_len--;
+		if (zxuno_dma_current_len==0) {
+			//Asumimos retrigger. TODO meter esto en funcion aparte que tambien lanza cuando se cambia dma_ctrl
+		    zxuno_dma_current_src=value_8_to_16(zxuno_dmareg[0][1],zxuno_dmareg[0][0]);
+            zxuno_dma_current_dst=value_8_to_16(zxuno_dmareg[1][1],zxuno_dmareg[1][0]);
+            zxuno_dma_current_len=value_8_to_16(zxuno_dmareg[3][1],zxuno_dmareg[3][0]);	
+		}
+}
+
 void zxuno_handle_dma(void)
 {
 	//Si esta la dma activada
@@ -187,18 +204,15 @@ void zxuno_handle_dma(void)
 
 	//Temp probar para dmaplay. dma_ctrl=7
 	z80_byte dma_source_value;
-	if (dma_ctrl==7) {
-		dma_source_value=peek_byte_no_time(zxuno_dma_current_src++);
-		printf ("out port %04XH value %02XH\n",zxuno_dma_current_dst,dma_source_value);
-		out_port_spectrum_no_time(zxuno_dma_current_dst,dma_source_value);
-
-		zxuno_dma_current_len--;
-		if (zxuno_dma_current_len==0) {
-			//Asumimos retrigger
-		    zxuno_dma_current_src=value_8_to_16(zxuno_dmareg[0][1],zxuno_dmareg[0][0]);
-            zxuno_dma_current_dst=value_8_to_16(zxuno_dmareg[1][1],zxuno_dmareg[1][0]);
-            zxuno_dma_current_len=value_8_to_16(zxuno_dmareg[2][1],zxuno_dmareg[2][0]);	
-		}
+	if (dma_ctrl==7) { 
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();
+		temp_dma_operate_memory_to_io();		
 	}
 
 
@@ -892,7 +906,10 @@ void zxuno_write_port(z80_int puerto, z80_byte value)
 				//DMACTRL
 				zxuno_dma_current_src=value_8_to_16(zxuno_dmareg[0][1],zxuno_dmareg[0][0]);
 				zxuno_dma_current_dst=value_8_to_16(zxuno_dmareg[1][1],zxuno_dmareg[1][0]);
-				zxuno_dma_current_len=value_8_to_16(zxuno_dmareg[2][1],zxuno_dmareg[2][0]);
+				zxuno_dma_current_len=value_8_to_16(zxuno_dmareg[3][1],zxuno_dmareg[3][0]);
+
+				printf ("Starting DMA src=%04XH dst=%02XH len=%04XH\n",zxuno_dma_current_src,zxuno_dma_current_dst,zxuno_dma_current_len);
+				sleep(3);
 			break;
 
 		}
