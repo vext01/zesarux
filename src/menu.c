283c7efbdbfc635bf23de0717ecf78bf1cbedc91
chernandezba <chernandezba@hotmail.com>
2018-08-23 12:31:18 +0200

    mc

--- src/menu.c
+++ src/menu.c
@@ -31836,7 +31836,7 @@ void menu_settings_display(MENU_ITEM_PARAMETERS)
 
 
 		if (MACHINE_IS_SPECTRUM || MACHINE_IS_ZX8081) {
-			menu_add_item_menu_format(array_menu_settings_display,MENU_OPCION_NORMAL,menu_display_osd_word_kb_length,NULL,"OSD Adventure KB length: %s",adventure_keyboard_key_length);
+			menu_add_item_menu_format(array_menu_settings_display,MENU_OPCION_NORMAL,menu_display_osd_word_kb_length,NULL,"OSD Adventure KB length: %d",adventure_keyboard_key_length);
 
 			menu_add_item_menu_tooltip(array_menu_settings_display,"Define the duration for every key press on the Adventure Text OSD Keyboard");
 			menu_add_item_menu_ayuda(array_menu_settings_display,"Define the duration for every key press on the Adventure Text OSD Keyboard, in 1/50 seconds (default 50)");
