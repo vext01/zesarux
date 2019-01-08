899620fd6dff0934c761b62177e1db499b01e564
chernandezba <chernandezba@hotmail.com>
2019-01-08 13:55:19 +0100

    mc

--- src/menu.c
+++ src/menu.c
@@ -32986,6 +32986,25 @@ void menu_filesel_cursor_abajo(void)
 
 }
 
+void zxvision_menu_filesel_cursor_abajo(zxvision_window *ventana)
+{
+
+	ventana->cursor_line++;
+	return;
+
+//ver que no sea ultimo archivo
+        if (si_menu_filesel_no_mas_alla_ultimo_item(filesel_linea_seleccionada)) {
+                                                //ver si es final de pantalla
+                                                if (filesel_linea_seleccionada==FILESEL_ALTO_DIR-1) {
+                                                        filesel_archivo_seleccionado++;
+                                                }
+                                                else {
+                                                        filesel_linea_seleccionada++;
+                                                }
+                                        }
+
+}
+
 void menu_filesel_cursor_arriba(void)
 {
 //ver que no sea primer archivo
@@ -33000,6 +33019,21 @@ void menu_filesel_cursor_arriba(void)
                                         }
 }
 
+void zxvision_menu_filesel_cursor_arriba(zxvision_window *ventana)
+{
+	 ventana->cursor_line--;
+        return;
+//ver que no sea primer archivo
+                                        if (filesel_archivo_seleccionado+filesel_linea_seleccionada!=0) {
+                                                //ver si es principio de pantalla
+                                                if (filesel_linea_seleccionada==0) {
+                                                        filesel_archivo_seleccionado--;
+                                                }
+                                                else {
+                                                        filesel_linea_seleccionada--;
+                                                }
+                                        }
+}
 
 
 //liberar memoria usada por la lista de archivos
@@ -34436,6 +34470,7 @@ int zxvision_menu_filesel(char *titulo,char *filtros[],char *archivo)
 
 	        ventana->upper_margin=4;
 	        ventana->lower_margin=4;
+		ventana->visible_cursor=1;
 
 		zxvision_draw_window(ventana);
 
@@ -34641,14 +34676,14 @@ int zxvision_menu_filesel(char *titulo,char *filtros[],char *archivo)
 				switch (tecla) {
 					//abajo
 					case 10:
-						menu_filesel_cursor_abajo();
+						zxvision_menu_filesel_cursor_abajo(ventana);
 						//Para no releer todas las entradas
 						menu_speech_tecla_pulsada=1;
 					break;
 
 					//arriba
 					case 11:
-						menu_filesel_cursor_arriba();
+						zxvision_menu_filesel_cursor_arriba(ventana);
 						//Para no releer todas las entradas
 						menu_speech_tecla_pulsada=1;
 					break;
@@ -34656,7 +34691,7 @@ int zxvision_menu_filesel(char *titulo,char *filtros[],char *archivo)
 					//PgDn
 					case 25:
 						for (aux_pgdnup=0;aux_pgdnup<FILESEL_ALTO_DIR;aux_pgdnup++)
-							menu_filesel_cursor_abajo();
+							zxvision_menu_filesel_cursor_abajo(ventana);
 						//releer todas entradas
 						menu_speech_tecla_pulsada=0;
 						//y decir active item
@@ -34666,7 +34701,7 @@ int zxvision_menu_filesel(char *titulo,char *filtros[],char *archivo)
 					//PgUp
 					case 24:
 						for (aux_pgdnup=0;aux_pgdnup<FILESEL_ALTO_DIR;aux_pgdnup++)
-							menu_filesel_cursor_arriba();
+							zxvision_menu_filesel_cursor_arriba(ventana);
 						//releer todas entradas
 						menu_speech_tecla_pulsada=0;
 						//y decir active item
@@ -34733,136 +34768,6 @@ int zxvision_menu_filesel(char *titulo,char *filtros[],char *archivo)
 					break;
 
 					case 13:
-						//Si se ha pulsado boton de raton
-						if (mouse_left){
-							//printf ("Boton pulsado\n");
-							//Si en linea de filtros
-							if (menu_mouse_y==FILESEL_POS_FILTER+1) {
-								/*menu_reset_counters_tecla_repeticion();
-								filesel_zona_pantalla=2;
-								break;*/
-							}
-
-							//Si en linea de "File"
-							if (menu_mouse_y==2) {
-								menu_reset_counters_tecla_repeticion();
-								filesel_zona_pantalla=0;
-								break;
-							}
-
-						//Si en zona derecha donde hay flechas arriba y abajo
-						if (menu_mouse_x==FILESEL_ANCHO-1 && menu_mouse_y==FILESEL_INICIO_DIR) {
-							debug_printf (VERBOSE_DEBUG,"Pressed cursor up button");
-
-							menu_filesel_cursor_arriba();
-						
-							//Para no releer todas las entradas
-							menu_speech_tecla_pulsada=1;
-
-							break;
-						}
-
-						//Si en zona derecha donde hay flechas arriba y abajo
-						if (menu_mouse_x==FILESEL_ANCHO-1 && menu_mouse_y==FILESEL_INICIO_DIR+FILESEL_ALTO_DIR+1) {
-							debug_printf (VERBOSE_DEBUG,"Pressed cursor down button");
-
-							menu_filesel_cursor_abajo();
-						
-							//Para no releer todas las entradas
-							menu_speech_tecla_pulsada=1;
-
-							break;
-						}
-
-
-							//Si se ha pulsado boton pero fuera de la zona de archivos, no hacer nada
-							if (!si_mouse_zona_archivos() ) break;
-						
-
-
-
-							//if (menu_mouse_y>=inicio_y_dir && menu_mouse_y<inicio_y_dir+FILESEL_ALTO_DIR) return 1;
-
-							//Si zona derecha (de indicador de porcentaje) y hay indicador de porcentaje activo
-							if (filesel_no_cabe_todo && menu_mouse_x==FILESEL_ANCHO-1) {
-								debug_printf (VERBOSE_DEBUG,"Mouse clicked percentage bar zone");
-								//printf ("Posicion: %d inicio_dir: %d\n",menu_mouse_y,FILESEL_INICIO_DIR);
-
-
-								int porcentaje_seleccionado=((menu_mouse_y-FILESEL_INICIO_DIR-1)*100)/FILESEL_ALTO_DIR;
-								//printf ("Porcentaje seleccionado: %d actual: %d\n",porcentaje_seleccionado,filesel_porcentaje_visible);	
-
-								debug_printf (VERBOSE_DEBUG,"Current percentage: %d Percentage clicked: %d",filesel_porcentaje_visible,porcentaje_seleccionado);
-						
-
-								//Si esta arriba del todo, cambiamos directamente la posicion
-								if (menu_mouse_y==FILESEL_INICIO_DIR+1) {
-									debug_printf (VERBOSE_DEBUG,"Forcing to first position");
-									filesel_linea_seleccionada=0;
-									filesel_archivo_seleccionado=0;
-									break;
-								}
-
-								if (menu_mouse_y==FILESEL_INICIO_DIR+FILESEL_ALTO_DIR) {
-									debug_printf (VERBOSE_DEBUG,"Forcing to last position");
-									//printf ("Forzamos a ultima posicion. Primero visible: %d. linea seleccionada: %d total: %d\n",filesel_archivo_seleccionado,filesel_linea_seleccionada,filesel_total_archivos);
-								
-									filesel_linea_seleccionada=FILESEL_ALTO_DIR-1;
-									filesel_archivo_seleccionado=filesel_total_archivos-1-filesel_linea_seleccionada;
-									//printf ("Archivo seleccionado: %d. Primero visible: %d\n",filesel_archivo_seleccionado,filesel_linea_seleccionada);
-
-									break;
-								}
-
-
-
-								int diferencia_porcentaje=porcentaje_seleccionado-filesel_porcentaje_visible;
-                            	if (diferencia_porcentaje>0) {
-			                        //Bajar cursor. cuanto?
-            	                    //indice_linea es el total de lineas
-                	                int lineas_bajar=(filesel_total_archivos*diferencia_porcentaje)/100;
-									 debug_printf (VERBOSE_DEBUG,"Going down %d lines",lineas_bajar);
-
-
-                        	        for (;lineas_bajar;lineas_bajar--) {
-                            	      menu_filesel_cursor_abajo();
-									}
-								//releer todas entradas
-								menu_speech_tecla_pulsada=0;
-								//y decir active item
-								menu_active_item_primera_vez=1;
-                                        
-               
-               		         	}
-							
-								if (diferencia_porcentaje<0) {
-    	                            //Subir cursor. cuanto?
-        	                        //indice_linea es el total de lineas
-									diferencia_porcentaje=-diferencia_porcentaje;
-                	                int lineas_subir=(filesel_total_archivos*diferencia_porcentaje)/100;
-									 debug_printf (VERBOSE_DEBUG,"Going up %d lines",lineas_subir);
-
-
-                        	        for (;lineas_subir;lineas_subir--) {
-                            	      menu_filesel_cursor_arriba();
-									}
-								//releer todas entradas
-								menu_speech_tecla_pulsada=0;
-								//y decir active item
-								menu_active_item_primera_vez=1;
-                                        
-               
-	           		        	}
-
-								break;
-
-							} 
-
-
-							//Si zona derecha (de indicador de porcentaje) aunque no hay indicador de porcentaje activo, no hacer nada
-							if (menu_mouse_x==FILESEL_ANCHO-1) break;
-
-						}
 
 						//si seleccion es directorio
 						item_seleccionado=menu_get_filesel_item(filesel_archivo_seleccionado+filesel_linea_seleccionada);
