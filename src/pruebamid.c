9bdcd21876501b413b1ca1bea91660386c96703f
chernandezba <chernandezba@hotmail.com>
2019-06-24 09:48:10 +0200

    mc

--- src/pruebamid.c
+++ src/pruebamid.c
@@ -2,28 +2,75 @@
 #include <string.h>
 
 
+//Convierte valor entero en variable length. Se devuelve en el orden tal cual tiene que ir en destino
+//Devuelve longitud en bytes
+int util_int_variable_length(unsigned int valor,unsigned char *destino)
+{
+
+    //Controlar rango maximo
+    if (valor>0x0FFFFFFF) valor=0x0FFFFFFF;
+
+    //Inicializarlo a cero
+    //destino[0]=destino[1]=destino[2]=destino[3]=0;
+    int longitud=1;
+
+    //Lo metemos temporalmente ahi
+    unsigned int final=0;
+
+    final=valor & 0x7F;
+
+    do {
+        valor=valor>>7;
+        if (valor) {
+            final=final<<8;
+            final |=(valor & 0x7f);
+            final |=128;
+            longitud++;
+        }
+    } while (valor);
+
+    //Y lo metemos en buffer destino
+    int i;
+    for (i=0;i<longitud;i++) { //maximo 4 bytes, por si acaso
+        unsigned char valor_leer=final & 0xFF;
+        final=final>>8;
+
+        destino[i]=valor_leer;
+    }
+
+    return longitud;
+
+
+}
+
+void dump_variable_length(unsigned char *buffer,int longitud)
+{
+    int i;
+    for (i=0;i<longitud;i++) {
+        printf ("%02X ",buffer[i]);
+    }
+    printf ("\n");
+}
 
 //http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
 
+
+//Devuelve longitud en bytes
 int mete_nota(unsigned char *mem,int duracion,int canal_midi,int keynote,int velocity)
 {
 
     int indice=0;
 
-    unsigned int deltatime=duracion & 0x7f; //de momento duracion variable de 1 byte
+    unsigned int deltatime=duracion;
 
-        //unsigned int deltatime=0x7f; //prueba
 
     //Evento note on. al momento
     mem[indice++]=0;
-    //midi_file[indice++]=(deltatime>>8)&0xFF;
-    
+   
 
     //int canal_midi=0;
     unsigned char noteonevent=(128+16) | (canal_midi & 0xf);
 
-    //unsigned char keynote=60; //C octava 4
-    //unsigned char velocity=100;
 
     mem[indice++]=noteonevent;
     mem[indice++]=keynote & 127;
@@ -32,8 +79,9 @@ int mete_nota(unsigned char *mem,int duracion,int canal_midi,int keynote,int vel
 
 
     //Evento note off
-    mem[indice++]=deltatime & 0xFF;
-    //midi_file[indice++]=(deltatime>>8)&0xFF;
+    int longitud_delta=util_int_variable_length(deltatime,&mem[indice]);
+    indice +=longitud_delta;
+
 
     unsigned char noteoffevent=(128) | (canal_midi & 0xf);
 
@@ -46,6 +94,55 @@ int mete_nota(unsigned char *mem,int duracion,int canal_midi,int keynote,int vel
 
 int main(void)
 {
+
+    //prueba variable length
+    unsigned char buffer_var_length[4];
+    int longitud;
+    longitud=util_int_variable_length(0,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);
+
+    longitud=util_int_variable_length(0x01,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);
+
+    longitud=util_int_variable_length(0x40,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x7f,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x80,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x00002000,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x00003FFF,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x00004000,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x00100000,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x001FFFFF,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x00200000,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+    longitud=util_int_variable_length(0x08000000,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);                            
+
+
+    longitud=util_int_variable_length(0x0FFFFFFF,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);    
+
+
+    longitud=util_int_variable_length(0xFFFFFFFF,buffer_var_length);
+    dump_variable_length(buffer_var_length,longitud);  
+
+  
 
     unsigned char midi_file[2048];
 
@@ -69,9 +166,13 @@ int main(void)
     midi_file[11]=pistas & 0xFF;   
 
  
-    //Division. TODO
+    //Division. Ticks per quarter note (redonda?)
+    //Haremos 400 (=50*4*2->redonda)
+
+    int division=100;
+
     midi_file[12]=0x00;
-    midi_file[13]=0x78;   
+    midi_file[13]=12;   
 
     //Pista
     memcpy(&midi_file[14],"MTrk",4);
@@ -79,14 +180,17 @@ int main(void)
 
     int notas=7;
 
-    int longitud_evento=((1+3)*2)*notas;//note on+off
+    //int longitud_evento=((1+3)*2)*notas;//note on+off
 
+    int puntero_longitud_pista=indice;
 
-    //longitud eventos
-    midi_file[indice++]=(longitud_evento>>24) & 0xFF;
+    //longitud eventos. meter al final
+    /*midi_file[indice++]=(longitud_evento>>24) & 0xFF;
     midi_file[indice++]=(longitud_evento>>16) & 0xFF;
     midi_file[indice++]=(longitud_evento>>8) & 0xFF;
-    midi_file[indice++]=(longitud_evento  ) & 0xFF;    
+    midi_file[indice++]=(longitud_evento  ) & 0xFF;    */
+
+    indice +=4;
 
     
     //TODO: averiguar parametro deltatime segun duracion
@@ -96,7 +200,15 @@ int main(void)
 
     //Nota 1
     
-    unsigned int deltatime=0x7f; //prueba
+    unsigned int deltatime=(division/2); ///2=redonda
+
+
+    deltatime=(division/2)/2; //blanca
+
+    deltatime=(division/2)/4; //negra
+
+    deltatime=(division/2)/8; //corchea
+
     int canal_midi=0;
 
     unsigned char keynote=60; //C octava 4
@@ -131,7 +243,20 @@ int main(void)
 
     //Nota 7
     keynote=71; //B octava 4
-    indice +=mete_nota(&midi_file[indice],deltatime,canal_midi,keynote,velocity);           
+    indice +=mete_nota(&midi_file[indice],deltatime,canal_midi,keynote,velocity);         
+
+
+    //Meter longitud eventos
+    int longitud_eventos=indice-puntero_longitud_pista-4; //evitar los 4 bytes que indican precisamente longitud
+
+    //longitud eventos. meter al final
+    midi_file[puntero_longitud_pista++]=(longitud_eventos>>24) & 0xFF;
+    midi_file[puntero_longitud_pista++]=(longitud_eventos>>16) & 0xFF;
+    midi_file[puntero_longitud_pista++]=(longitud_eventos>>8) & 0xFF;
+    midi_file[puntero_longitud_pista++]=(longitud_eventos  ) & 0xFF;    
+
+        
+
 
 
          FILE *ptr_configfile;
