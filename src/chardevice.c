4f5f997859a8125c0442baa43d03170f1fae20e0
chernandezba <chernandezba@hotmail.com>
2019-07-12 13:46:11 +0200

    mc

--- src/chardevice.c
+++ src/chardevice.c
@@ -69,8 +69,10 @@ int chardevice_open(char *path,enum chardevice_openmode mode)
     if (handler>=0) {
         //Agregar no bloqueo. manera estandard
         //Haciendolo con el open tambien seria valido, pero mejor asi
+#ifndef MINGW        
         int flags = fcntl(handler, F_GETFL, 0);
         fcntl(handler, F_SETFL, flags | O_NONBLOCK);
+#endif
     }
 
     return handler;
