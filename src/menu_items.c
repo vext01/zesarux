6fb30817e748251930ba8d9886b35253da5c6e02
Cesar Hernandez Hotmail Hotmail <chernandezba@hotmail.com>
2019-08-22 16:00:41 +0200

    added acknowledgements to ZXwebmaster

--- src/menu_items.c
+++ src/menu_items.c
@@ -16868,7 +16868,12 @@ void menu_network(MENU_ITEM_PARAMETERS)
 #endif
              menu_add_item_menu_format(array_menu_common,MENU_OPCION_NORMAL,menu_network_http_request,NULL,"Http request");         
              
-              menu_add_item_menu_format(array_menu_common,MENU_OPCION_NORMAL,menu_online_browse_zx81,NULL,"zx81 online browser");  
+              			menu_add_item_menu_format(array_menu_common,MENU_OPCION_NORMAL,menu_online_browse_zx81,NULL,"zx81 online browser");  
+             
+             menu_add_item_menu_tooltip(array_menu_common,"Connects to the www.zx81.nl site to download ZX81 games. Many thanks to ZXwebmaster for allowing it"); 
+             
+              menu_add_item_menu_ayuda(array_menu_common,"Connects to the www.zx81.nl site to download ZX81 games. Many thanks to ZXwebmaster for allowing it"); 
+              
 						
 			menu_add_item_menu(array_menu_common,"",MENU_OPCION_SEPARADOR,NULL,NULL);
 
