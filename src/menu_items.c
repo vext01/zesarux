6cf79543f312438b228ab8cf923d579d53e7a5a3
Cesar Hernandez Hotmail Hotmail <chernandezba@hotmail.com>
2019-08-30 15:59:40 +0200

    improve wos search filter

--- src/menu_items.c
+++ src/menu_items.c
@@ -17113,7 +17113,7 @@ void menu_online_browse_zxinfowos(MENU_ITEM_PARAMETERS)
 {
 	//char oldletra=s_online_browse_zx81_letra[0];
 	
-	char query_search[256];
+	char query_search[1024];
 	query_search[0]=0;
 	
 	menu_ventana_scanf("Query",query_search,256);
@@ -17123,7 +17123,7 @@ void menu_online_browse_zxinfowos(MENU_ITEM_PARAMETERS)
 	//http://a.zxinfo.dk/api/zxinfo/v2/search?query=head%20over%20heels&mode=compact&sort=rel_desc&size=10&offset=0
 
 	char query_url[1024];
-	sprintf (query_url,"/api/zxinfo/v2/search?query=%s&mode=compact&sort=rel_desc&size=100&offset=0&contenttype=SOFTWARE",query_search);
+	sprintf (query_url,"/api/zxinfo/v2/search?query=%s&mode=compact&sort=rel_desc&size=100&offset=0&contenttype=SOFTWARE&availability=Available",query_search);
 
 	char query_id[256];
 	menu_online_browse_zxinfowos_query(query_id,"a.zxinfo.dk",query_url,"hits.","_id=","fulltitle=");
