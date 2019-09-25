3fc2ad0d4645341bf9bb08e88a6e8629c8b82a13
chernandezba <chernandezba@hotmail.com>
2019-09-25 20:54:12 +0200

    mc

--- src/syncsnap.sh
+++ src/syncsnap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #simple test to show snapshot copy from one instance of ZEsarUX to another
 #the second instance has ZRCP port 10010
+#this functionality is now done using ZENG menus,
+#this script is only created to show a simple concept and alternative way to do it
 
 TEMPFILE=`mktemp`
 ( sleep 1 ; echo "get-snapshot " ; sleep 1 ) | telnet localhost 10000 > $TEMPFILE
